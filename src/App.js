1805c7c706420e5149b5c87794e74cffb29fc166
Shivam Rai <200shivamrai@gmail.com>
2023-09-24 23:30:30 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Student from "./Student";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Student name={"Shivam"} email={"shivamrai@gmail.com"} />
+      <Student name={"Sawan"} email={"@sawangmail.com"} />
     </div>
   );
 }
 
-export default App;
